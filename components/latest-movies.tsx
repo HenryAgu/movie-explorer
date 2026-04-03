12b7366604a638115b8239cf96f2cee20d8fbea0
Henry Agu <aguhenrychuks@gmail.com>
2026-04-03 03:24:51 +0100

    feat: switched homepage movies from trending to popular

--- components/latest-movies.tsx
+++ components/latest-movies.tsx
@@ -1,12 +1,12 @@
 import { Movie } from "@/type"
 import { MovieCard } from "./movie-card"
 
-const LatestMovies = ({ trending }: { trending: Movie[] }) => {
+const LatestMovies = ({ popular }: { popular: Movie[] }) => {
     return (
         <div className='xl:px-16 px-5 py-20'>
             <p className="text-3xl sm:text-4xl font-black text-black leading-tight tracking-tight">Latest Movies</p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-10 gap-5 lg:gap-10">
-                {trending.slice(0, 20).map(movie => (
+                {popular.slice(0, 20).map(movie => (
                     <MovieCard key={movie.id} movie={movie} />
                 ))}
             </div>
